3c1a833a1d6444c2aa818b380abacda4f6e21278
Pim Coumans <pim@pixelrock.nl>
2016-11-25 14:46:00 +0100

    Initial Commit

---
+++ Metal2DTests/Metal2DTests.swift
@@ -0,0 +1,36 @@
+//
+//  Metal2DTests.swift
+//  Metal2DTests
+//
+//  Created by Pim Coumans on 25/11/16.
+//  Copyright © 2016 pixelrock. All rights reserved.
+//
+
+import XCTest
+@testable import Metal2D
+
+class Metal2DTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
