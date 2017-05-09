07426bb7bc1d72f301b4e1273ea6811a0e5b8c18
Pim Coumans <pim@pixelrock.nl>
2017-05-09 12:25:44 +0200

    Removed Storyboard from project

--- Metal2DiOS/AppDelegate.swift
+++ Metal2DiOS/AppDelegate.swift
@@ -12,9 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+	var viewController: UIViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		self.window = UIWindow(frame: UIScreen.main.bounds)
+		viewController = GLRenderViewController()
+		if let viewController = viewController {
+			self.window?.rootViewController = viewController
+		}
+		self.window?.makeKeyAndVisible()
         // Override point for customization after application launch.
         return true
     }
